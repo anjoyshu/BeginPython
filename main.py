3e284b38b02eabe93d53b4fc2dfb872d1a3f718d
captainH <34805913+anjoyshu@users.noreply.github.com>
2022-02-22 05:44:35 +0000

    增加註解

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-exec(open("guess.py").read())
-exec(open("9x9.py").read())
-exec(open("dict.py").read())
+#exec(open("guess.py").read())
+#exec(open("9x9.py").read())
+#exec(open("dict.py").read())
 exec(open("worm.py").read())
